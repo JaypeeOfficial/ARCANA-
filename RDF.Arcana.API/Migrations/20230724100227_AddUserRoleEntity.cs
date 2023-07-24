2cff7f97a7b24e6dbfc183bd0cebcebad0d0eec4
aldrin0408av <aldrinmundovega@gmail.com>
2023-07-24 18:48:00 +0800

    "Refactor User entity and add UserRole entity"
    Updated User and UserRole entities to reflect relationship changes in data model.
    
    User entity now includes a non-nullable 'user_role_id' field replacing the 'user_id' field in 'user_roles' and the 'role_id' in 'users' is now nullable. These changes offer better consistency and clarity regarding the relationship between User and UserRole entities.
    
    The migration also adds UserRole entity, representing the newly introduced UserRole table in the database. This table will store details about roles assigned to users which was previously stored in the User entity itself. This change provides more flexibility and scalability for the system in assigning and managing user roles.

---
+++ RDF.Arcana.API/Migrations/20230724100227_AddUserRoleEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace RDF.Arcana.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRoleEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    role_name = table.Column<string>(type: "longtext", nullable: true),
+                    permissions = table.Column<string>(type: "longtext", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    is_active = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_roles", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_roles");
+        }
+    }
+}
---
+++ RDF.Arcana.API/Migrations/20230724102618_AdjustUserEntity.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RDF.Arcana.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_users_roles_role_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "user_roles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "role_id",
+                table: "users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "user_role_id",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_user_role_id",
+                table: "users",
+                column: "user_role_id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_users_roles_role_id",
+                table: "users",
+                column: "role_id",
+                principalTable: "roles",
+                principalColumn: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_users_user_roles_user_role_id",
+                table: "users",
+                column: "user_role_id",
+                principalTable: "user_roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_users_roles_role_id",
+                table: "users");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_users_user_roles_user_role_id",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "ix_users_user_role_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "user_role_id",
+                table: "users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "role_id",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "user_id",
+                table: "user_roles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_users_roles_role_id",
+                table: "users",
+                column: "role_id",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
