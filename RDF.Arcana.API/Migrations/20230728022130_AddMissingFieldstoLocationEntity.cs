c1a539bc15885f686ccb8a59482c3151ec6c8561
aldrin0408av <aldrinmundovega@gmail.com>
2023-07-28 10:25:17 +0800

    Add missing fields to Location Entity
    Added 'added_by' and 'modified_by' fields in the location, department and company tables in the database. This was required to track which user made changes to these entities, enhancing traceability.

---
+++ RDF.Arcana.API/Migrations/20230728022130_AddMissingFieldstoLocationEntity.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RDF.Arcana.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingFieldstoLocationEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "added_by",
+                table: "locations",
+                type: "longtext",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "modified_by",
+                table: "locations",
+                type: "longtext",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "added_by",
+                table: "departments",
+                type: "longtext",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "modified_by",
+                table: "departments",
+                type: "longtext",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "added_by",
+                table: "companies",
+                type: "longtext",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "modified_by",
+                table: "companies",
+                type: "longtext",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "added_by",
+                table: "locations");
+
+            migrationBuilder.DropColumn(
+                name: "modified_by",
+                table: "locations");
+
+            migrationBuilder.DropColumn(
+                name: "added_by",
+                table: "departments");
+
+            migrationBuilder.DropColumn(
+                name: "modified_by",
+                table: "departments");
+
+            migrationBuilder.DropColumn(
+                name: "added_by",
+                table: "companies");
+
+            migrationBuilder.DropColumn(
+                name: "modified_by",
+                table: "companies");
+        }
+    }
+}
